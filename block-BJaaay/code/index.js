ed815414e2633209578f5f42bea303c2b596b877
Akhil Kumar <akhilkum249@gmail.com>
2023-11-17 05:38:12 +0530

    Assignment: Loops completed

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -4,10 +4,30 @@
 
 // Using `console.log` log all the values from 1 to 10.
 
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
+
 // Using `console.log` log all the evan values from 1 to 10.
-
-// Using `console.log` log all the od values from 1 to 10.
-
+for(i=1;i<=10;i++){
+    if(i%2===0){
+        console.log(i);
+    }
+}
+// Using `console.log` log all the odd values from 1 to 10.
+for(i=1;i<=10;i++){
+    if(i%2!==0){
+        console.log(i);
+    }
+}
 // Calculate the sum of all numbers from 1 to 10.
-
+let sum=0
+for(i=1;i<=10;sum=i++){
+    console.log(`The addition of first 10 numbers are:${sum}`);
+}
 // Log all the values from 1 to 10 using while loop
+let j=0;
+while(j<=10){
+    console.log(j);
+    j++;
+}
